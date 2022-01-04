95d8f713260daa88ba04fbbbaf8722dc5d061609
Magdy <magdy@Magdy-MacBook-Pro.local>
2022-01-04 14:35:47 +0200

    fix: password length

--- migrations/sqls/20211227033201-users-table-up.sql
+++ migrations/sqls/20211227033201-users-table-up.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY, 
   firstname VARCHAR(50) NOT NULL, 
   lastname VARCHAR(50) NOT NULL, 
-  password VARCHAR(1000) NOT NULL
+  password VARCHAR(100) NOT NULL
   );
