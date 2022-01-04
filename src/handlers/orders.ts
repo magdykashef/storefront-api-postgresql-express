32b0b4a19c76774458c16cd6e17b4c6385bc5aca
Magdy <magdy@Magdy-MacBook-Pro.local>
2022-01-04 14:39:42 +0200

    add: /orders/:id/products/ route

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -8,7 +8,7 @@ const show = async (req: Request, res: Response) => {
   try {
     const order = await store.show(parseInt(req.params.id));
     if (!order) {
-      return res.status(400).json({error:'order is not found'});
+      return res.status(400).json({ error: 'order is not found' });
     }
     return res.status(200).json(order);
   } catch (error) {
@@ -19,10 +19,8 @@ const show = async (req: Request, res: Response) => {
 const create = async (req: Request, res: Response) => {
   try {
     const order = await store.create({
-      product_id: parseInt(req.body.product_id),
-      quantity: parseInt(req.body.quantity),
       user_id: parseInt(req.body.user_id),
-      status: req.body.status,
+      status: req.body.status
     });
     return res.status(200).json(order);
   } catch (error) {
@@ -31,22 +29,23 @@ const create = async (req: Request, res: Response) => {
   }
 }
 
-// const addProduct = async (req: Request, res: Response) => {
-//   const orderId: string = req.params.id;
-//   const productId: string = req.body.productId;
-//   const quantity: number = parseInt(req.body.quantity);
-
-//   try {
-//     const addedProduct = await store.addProduct(quantity, +orderId, +productId);
-//     res.status(201).json(addedProduct);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// }
+const addProduct = async (req: Request, res: Response) => {
+  const order_id: number = parseInt(req.body.order_id);
+  const product_id: number = parseInt(req.body.product_id);
+  const quantity: number = parseInt(req.body.quantity);
+  
+  try {
+    const addedProduct = await store.addProduct(order_id, product_id, quantity);
+    res.status(201).json(addedProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
 
 const orderRoutes = (app: express.Application) => {
-  app.post('/orders', verifyAuthToken, create);
   app.get('/orders/:id', verifyAuthToken, show);
+  app.post('/orders', verifyAuthToken, create);
+  app.post('/orders/:id/products', verifyAuthToken, addProduct);
 }
 
 export default orderRoutes;
